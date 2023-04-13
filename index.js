784969b6e0fd1745f024f40cda894797cdca990b
pabon98 <hasinanjumpabon@gmail.com>
2023-04-14 00:03:21 +0600

    code updated

--- index.js
+++ index.js
@@ -3,36 +3,150 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId
 const port = process.env.Port || 5000
 
+//Cors policy
 app.use(cors())
+app.use(express.json())
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzm3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majorit`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lzm3u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// console.log(uri);
 
-async function run(){
-    try{
-        await client.connect()
-        const database = client.db('creativeAgency')
-        const serviceCollection = database.collection("services")
 
-        //Get services api
-        app.get('/services', async(req,res)=>{
-         const cursor = serviceCollection.find({})
-         const services = await cursor.toArray()
-         res.send(services)
-        })
-    }
-    finally{
-    //   await client.close()
-    }
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("creativeAgency");
+    const serviceCollection = database.collection("services");
+    const orderCollection = database.collection("orders");
+    const reviewCollection = database.collection("review");
+    const usersCollection = database.collection("users");
+
+    //get cycle api
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.json(service);
+    });
+    // GET API (get all orders)
+    app.get("/orders", async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    // POST API
+    app.post("/placeOrder", async (req, res) => {
+      const orderDetails = req.body;
+      console.log(orderDetails);
+      const result = await orderCollection.insertOne(orderDetails);
+      res.json(result);
+    });
+    // GET API (get orders by email)
+    app.get("/myOrders/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = await orderCollection.find(query);
+      const myOrders = await cursor.toArray();
+      res.send(myOrders);
+    });
+
+    // DELETE API
+    app.delete("/deleteOrder/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
+    //GET Review API
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+    // POST API(add a new review)
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.json(result);
+    });
+    app.put("/makeAdmin", async (req, res) => {
+      const filter = { email: req.body.email };
+      const result = await usersCollection.find(filter).toArray();
+      if (result) {
+        const documents = await usersCollection.updateOne(filter, {
+          $set: { role: "admin" },
+        });
+        console.log(documents);
+      }
+      
+    });
+    //add user
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+
+  app.post("/addUserInfo", async (req, res) => {
+    console.log("req.body");
+    const result = await usersCollection.insertOne(req.body);
+    res.send(result);
+    console.log(result);
+  });
+    // check admin or not
+    app.get("/checkAdmin/:email", async (req, res) => {
+      const result = await usersCollection
+        .find({ email: req.params.email })
+        .toArray();
+      // console.log(result);
+      res.send(result);
+    });
+    // DELETE API (delete product by id)
+    app.delete("/deleteProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await cycleCollection.deleteOne(query);
+      res.json(result);
+    });
+    // UPDATE API
+    app.put("/approve/:id", async (req, res) => {
+      const id = req.params.id;
+      const approvedOrder = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          status: approvedOrder.status,
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+  } 
+  finally {
+    //await client.close()
+  }
 }
-
-app.get('/', (req, res) => {
-  res.send('Hello Creative Agency!')
-})
+run().catch(console.dir);
+app.get("/", (req, res) => {
+  res.send("Hello creative agency!");
+});
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`)
-})
+  console.log(`listening at ${port}`);
+});
